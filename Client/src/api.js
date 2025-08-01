73c9a664b490f9556e79bdd36a5d0a15e35716a9
Saksdev <sakshams643@gmail.com>
2025-08-01 17:29:14 +0530

    update testing api url

--- Client/src/api.js
+++ Client/src/api.js
@@ -1,4 +1,5 @@
-const API_URL = 'http://localhost:5000/api';
+const API_URL = import.meta.env.VITE_API_URL;
+// const API_URL = 'http://localhost:5000/api';
 
 // Register a new user
 export async function registerUser(data) {
